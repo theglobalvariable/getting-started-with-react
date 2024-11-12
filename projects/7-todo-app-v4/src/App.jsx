4d34c6dadf8242688b302b739d4ad8a571a7f47a
Babu Swami <swamibabulal@outlook.com>
2024-11-12 12:41:29 +0530

    feat: todo app using forms (#1)

---
+++ projects/7-todo-app-v4/src/App.jsx
@@ -0,0 +1,14 @@
+import AppName from "./components/AppName";
+import "./App.css";
+import TodoList from "./components/TodoList";
+
+function App() {
+  return (
+    <center>
+      <AppName />
+      <TodoList />
+    </center>
+  );
+}
+
+export default App;
---
+++ projects/7-todo-app-v4/src/components/TodoItem.jsx
@@ -0,0 +1,32 @@
+import PropTypes from "prop-types";
+import styles from "./TodoList.module.css";
+import { AiFillDelete } from "react-icons/ai";
+
+const TodoItem = ({ id, title, date, onDeleteClickHandler }) => {
+  return (
+    <div className="container">
+      <div className={`row ${styles["my-row"]}`}>
+        <div className="col-6">{title}</div>
+        <div className="col-4">{date}</div>
+        <div className="col-2">
+          <button
+            type="button"
+            className={`btn btn-danger ${styles["my-button"]}`}
+            onClick={() => onDeleteClickHandler(id)}
+          >
+            <AiFillDelete />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+TodoItem.propTypes = {
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string,
+  date: PropTypes.string,
+  onDeleteClickHandler: PropTypes.func,
+};
+
+export default TodoItem;
